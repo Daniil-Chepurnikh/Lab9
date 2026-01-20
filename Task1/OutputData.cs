ec3e56b5b12f72a66c11c5bc87c3f4da9af4fa6b
Daniil Chepurnikh <chepurnikh.daniil@gmail.com>
2026-01-21 03:56:39 +0500

    Сделал вывод ошибок понятнее и лаконичнее

--- Task1/OutputData.cs
+++ Task1/OutputData.cs
@@ -8,18 +8,13 @@ namespace Task1
         /// Печатает ошибки
         /// </summary>
         /// <param name="error">Печатаемая ошибка</param>
-        public static void Error(string error = "Введено недопустимое значение")
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Ошибка: " + error + '!');
-            Console.ResetColor();
-        }
+        public static void Error(string error) => Message($"Ошибка: {error}!", ConsoleColor.Red);
 
         /// <summary>
         /// Печатаеn красивые сообщения
         /// </summary>
         /// <param name="message">Сообщение на печать</param>
-        /// <param name="color">Цвет печать</param>
+        /// <param name="color">Цвет текста</param>
         public static void Message(string message = "Ввод корректен \n", ConsoleColor color = ConsoleColor.White)
         {
             Console.ForegroundColor = color;
