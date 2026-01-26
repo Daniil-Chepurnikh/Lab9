43e2a3c8d69360128d8ad630b2ff34bdcadde52b
Daniil Chepurnikh <chepurnikh.daniil@gmail.com>
2026-01-27 00:31:41 +0500

    Оставил комментарии

--- Task1/PokemonArray.cs
+++ Task1/PokemonArray.cs
@@ -6,23 +6,33 @@ namespace Task1
     {
         Pokemon[] pokemons;
 
+        /// <summary>
+        /// Инициализация по умолчанию
+        /// </summary>
         public PokemonArray() 
         {
             // TODO: Разобраться по материалам как сделать конструктор без параметров
         }
 
-
+        /// <summary>
+        /// Инициализация случайными значениями элементов массива
+        /// </summary>
+        /// <param name="length">Длина массива</param>
         public PokemonArray(int length)
         {
             // TODO: Разобраться по материалам как заполнять элементы случайными числами
         }
 
+        /// <summary>
+        /// Копирование
+        /// </summary>
+        /// <param name="pokemons">Копируемый массив</param>
         public PokemonArray(PokemonArray pokemons)
         {
             // TODO: Разобраться по материалам как сделать глубокое копирование
         }
 
-        // TODO: метод для просмотра элементов массива
+        // TODO: Написать метод для просмотра элементов массива
 
     }
 }
