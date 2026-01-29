06ec6b78fc5c9fe98da25f7cd9e158e4680b4b6a
Daniil Chepurnikh <chepurnikh.daniil@gmail.com>
2026-01-30 03:15:30 +0500

    Подготовил всё для написания тестов

--- Testing/Test1.cs
+++ Testing/Test1.cs
@@ -1,11 +1,14 @@
-﻿namespace Testing
+﻿using Task;
+
+namespace Testing
 {
     [TestClass]
     public sealed class Test1
     {
         [TestMethod]
         public void TestMethod1()
-        {
+        { 
+            Pokemon p  = new();
         }
     }
 }
